dbbc23d11874557e166c17c5153776721f69c4cb
temiyores <temiyores1234@gmail.com>
2023-08-01 18:37:31 +0100

    updated projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,39 +1,57 @@
 import React from "react";
 import "./portfolio.css";
 import { Container, Row, Col } from "reactstrap";
-import img1 from "../../assets/images/port1.png";
-import img2 from "../../assets/images/port2.png";
-import img3 from "../../assets/images/port3.png";
-import img4 from "../../assets/images/flappy.png";
+import birthday from "../../assets/images/birthday.png";
+import novarickhomes from "../../assets/images/novarickhomes.png";
+import flappybird from "../../assets/images/flappybird.png";
 import img5 from "../../assets/images/novagro.jpeg";
 import img6 from "../../assets/images/crypto.jpeg";
-
+import wingman from "../../assets/images/wingman.png";
+import restaurant from "../../assets/images/restaurant.png";
+import plugskills from "../../assets/images/plugskills.png";
+import mernauth from "../../assets/images/mernauth.png";
 const portfolioData = [
   {
-    imgUrl: img1,
-    title: "Aptech Abeokuta Website",
-    url: "http://www.aptechabeokuta.com.ng/",
+    imgUrl: restaurant,
+    title: "Ile Iyan Restaurant",
+    url: "https://ileiyan.ng/",
   },
   {
-    imgUrl: img2,
-    title: "Html, Css and Javascipt Website",
-    url: "https://thecake-shop.netlify.app/",
-  },
-  {
-    imgUrl: img3,
+    imgUrl: novarickhomes,
     title: "Novarick Homes and Properties Website ",
     url: "https://novarickhomes.netlify.app/",
-  },
-  {
-    imgUrl: img4,
-    title: "Flappy Flop Game",
-    url: "https://flappyflop.netlify.app/",
   },
   {
     imgUrl: img5,
     title: "Novarick Agro Limited Website",
     url: "https://novarickagro.netlify.app/",
   },
+  {
+    imgUrl: wingman,
+    title: "Wingman Legal Website",
+    url: "https://wingman.legal/",
+  },
+  {
+    imgUrl: plugskills,
+    title: "PlugSkills Website",
+    url: "https://plugskills.com/",
+  },
+  {
+    imgUrl: mernauth,
+    title: "Mern Authentication",
+    url: "https://qayyumauthentication.netlify.app/",
+  },
+  {
+    imgUrl: birthday,
+    title: "Cake Website",
+    url: "https://thecake-shop.netlify.app/",
+  },
+  {
+    imgUrl: flappybird,
+    title: "Flappy Flop Game",
+    url: "https://flappyflop.netlify.app/",
+  },
+
   {
     imgUrl: img6,
     title: "cryptoVerse Website",
@@ -49,8 +67,7 @@ const Portfolio = () => {
           <Col lg="12" className="portfolio_top mb-3">
             <h2 className="mb-1">Portfolio</h2>
             <h6>
-              Explore my work and give feedback, Also some projects are not
-              mobile responsive yet.
+              Explore my work and give feedback in the contact form. Thank you.
             </h6>
           </Col>
 
@@ -75,7 +92,28 @@ const Portfolio = () => {
           ))}
         </Row>
         <Row className="mt-5">
-          {portfolioData.slice(3, 7).map((item, index) => (
+          {portfolioData.slice(3, 6).map((item, index) => (
+            <Col lg="4" md="6" sm="6" key={index}>
+              <div className="portfolio_card">
+                <div className="portfolio_img">
+                  <img src={item.imgUrl} alt="" className="w-100" />
+                </div>
+
+                <a
+                  className="portfolio_content"
+                  href={item.url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <h5>{item.title}</h5>
+                  <span> View live demo</span>
+                </a>
+              </div>
+            </Col>
+          ))}
+        </Row>
+        <Row className="mt-5">
+          {portfolioData.slice(6, 10).map((item, index) => (
             <Col lg="4" md="6" sm="6" key={index}>
               <div className="portfolio_card">
                 <div className="portfolio_img">
